c354d16f36f19015f40719c593f6a40f7d871f12
Derek <dtuggle1@gmail.com>
2025-04-25 15:29:15 -0700

    Filter class first pass

--- Backend/main.cpp
+++ Backend/main.cpp
@@ -8,38 +8,57 @@ using json = nlohmann::json;
 class Filters {
     private:
         std::string keyword;
-        float:: min_rating;
+        float min_rating;
         std::string location_latitude;
         std::string location_longittude;
         float distance;
-        float min_reviews;
+        int min_reviews;
         std::string price;
-        std::string open_hour
+        std::string open_hour;
+        json primary_search_filters;
+
         // json object data type
-    public:
+
         void loadJsonFromFile(const std::string& filename){
+            std::ifstream file(filename);
+            if (!file.is_open()){
+                throw std::runtime_error("Couldnt open file");
+            }
+            json json_file;
+            file >> json_file;
 
-        }
-        void allFilters(){
-
+            keyword = json_file.at("keyword").get<std::string>();
+            min_rating = json_file.at("min_rating").get<int>();
+            location_latitude = json_file.at("location_latitude").get<std::string>();
+            location_longitude = json_file.at("location_longitude").get<std::string>();
+            distance = json_file.at("distance").get<float>();
+            min_reviews = json_file.at("min_reviews").get<int>();
+            price = json_file.at("price").get<std::string>();
+            open_hour = json_file.at("open_hour").get<std::string>();
         }
         void primarySearchFilters(){
             
-            std::ostringstream coordinates = "" // todo:update
+            std::string coordinates = std::format("{},{}", location_latitude, location_longittude);
             // todo: change payload vars to the correct ones per the api
             json payload = {
                 {"keyword", this->keyword},
                 {"coordinates", coordinates}, 
                 {"distance", this->distance}
             }
-
+            primary_search_filters = payload;
         }
-
-
+    public:
+        Filters(const std::string& filename){
+            loadJsonFromFile(filename);
+            primarySearchFilters();
+        }
+        json getPrimarySearchFilters(){
+            return primary_search_filters;
+        }
 }
 
 json primarySearch(Filters filters){
-    std::ostringstream url;
+    std::string url;
     std::string payload;
     url = "https://serpapi.com/searches/625535ed92cebc19/6807b9b6a919a9218a7d85e7.json"
     std::cout << "Attempting to Access API" << "\n";
@@ -72,8 +91,8 @@ json primarySearch(Filters filters){
 
 int main() {
     std:: string filename = "sample-filters.json";
-    Filters filters;
-    filters.loadJsonFromFile(filename&);
+    Filters filters(filename);
+
 
 
 }
